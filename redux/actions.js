ab994108f44a0d7fa3eefd188e2dd52ec6a2e300
Bruno Guerra <brunoguerrait@gmail.com>
2016-09-25 13:06:56 -0300

    actions creation helper

--- redux/actions.js
+++ redux/actions.js
@@ -3,18 +3,19 @@ const types = [
   'SUCCESS'
 ]
 
-const capitilze = (str) => str.charAt(0).toUpperCase() + str.substr(1);
+const capitalize = (str) =>
+  str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
 const actionName = (action, type) =>
   `${action.toUpperCase()}_${type.toUpperCase()}`;
 
 const createAction = (action) => {
-  return types.reduce((res, cur) => Object.assign({
-    [`${action}${capitalize(cur)}`]: (payload) => {
+  return types.reduce((res, cur) => Object.assign(res, {
+    [`${action}${capitalize(cur)}`]: (payload) => ({
       type: actionName(action, cur),
       payload
-    }
+    })
   }), {})
-}
+};
 
 export const bootAction = () => ({
   type: 'BOOT_ACTION'
@@ -23,3 +24,13 @@ export const bootAction = () => ({
 export const counterAction = () => ({
   type: 'INCREMENT'
 });
+
+const checkNews = createAction('checkNews');
+export const actions = {
+  checkNews
+}
+
+// debugging action creation
+const newAction = createAction('sample');
+console.log('Action creation', newAction);
+console.log('Action creator call', newAction.sampleRequest());
